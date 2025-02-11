3279d50e11d6e2fb9d7719813c32c2b09d1fa705
rinas21 <mohomadrinas00@gmail.com>
2025-02-11 10:41:46 +0530

    solved

--- leetcode-412-fizzbuzz.js
+++ leetcode-412-fizzbuzz.js
@@ -4,21 +4,21 @@
  */
 var fizzBuzz = function(n) {
     let result=[];
-    for (let i=1; i < result.length; i++) {
+    for (let i=1; i <=n ;i++) {
        
     if (i%15===0) {
         result.push("FizzBuzz");
     } else if(i%3===0) {
         result.push("Fizz");
     }
-    else if(n%i===0){
+    else if(i%5===0){
         result.push("Buzz");
     }
     else{
-        result.push(n.toString());
+        result.push(i.toString());
     } 
     }
     return result;
     };
 
-fizzBuzz(5);
+    console.log(fizzBuzz(5));
