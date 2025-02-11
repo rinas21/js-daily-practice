e84fe4c93412995b2c73e1f69f73bbc06a15ec9a
rinas21 <mohomadrinas00@gmail.com>
2025-02-11 15:08:53 +0530

    editot

--- leetcode-412-fizzbuzz.js
+++ leetcode-412-fizzbuzz.js
@@ -4,23 +4,23 @@
  * @param {number} n
  * @return {string[]}
  */
-var fizzBuzz = function(n) {
-    let result=[];
-    for (let i=1; i <=n ;i++) {
-       
-    if (i%15===0) {
-        result.push("FizzBuzz");
-    } else if(i%3===0) {
-        result.push("Fizz");
-    }
-    else if(i%5===0){
-        result.push("Buzz");
-    }
-    else{
-        result.push(i.toString());
-    } 
+var fizzBuzz = function (n) {
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+
+        if (i % 15 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        }
+        else if (i % 5 === 0) {
+            result.push("Buzz");
+        }
+        else {
+            result.push(i.toString());
+        }
     }
     return result;
-    };
+};
 
-    console.log(fizzBuzz(5));
+console.log(fizzBuzz(5));
